2f8b699c98efb2e9c05e109bcf6ed44c1dbc7be0
levymc <levymcruz@gmail.com>
2023-05-09 19:29:39 -0300

    adicionando o componentes Letras

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,11 +1,13 @@
 import Jogo from "./Jogo";
+import Letras from "./Letras";
 
 
 function App() {
   const forca0 = process.env.PUBLIC_URL + '/assets/forca0.png';
   return (
-    <div className="container flex centerAll">
+    <div className="container flex">
       <Jogo />
+      <Letras />
     </div>
   );
 }
--- src/componentes/Letras.js
+++ src/componentes/Letras.js
@@ -1,4 +1,17 @@
 
-export default function Letras(){
+let Letra = () => {
 
 }
+
+export default function Letras() {
+    const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+    return (
+      <div className="divLetras flex centerAll">
+        {alfabeto.map((letra, index) => (
+          <div key={index} className="letra">{letra}</div>
+        ))}
+      </div>
+    );
+  }
+  
