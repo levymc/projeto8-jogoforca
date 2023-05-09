05b4273b254690e742736a0559028f11766dc082
levymc <levymcruz@gmail.com>
2023-05-09 19:37:44 -0300

    estilizando as letras e criando a formatacao

--- src/componentes/Letras.js
+++ src/componentes/Letras.js
@@ -7,9 +7,9 @@ export default function Letras() {
     const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   
     return (
-      <div className="divLetras flex centerAll">
+      <div className="divLetras flex">
         {alfabeto.map((letra, index) => (
-          <div key={index} className="letra">{letra}</div>
+          <div key={index} className="letra flex">{letra}</div>
         ))}
       </div>
     );
