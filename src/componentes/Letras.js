5f868e7fe17e1ef52d2f19540bf9e912916f09de
levymc <levymcruz@gmail.com>
2023-05-13 18:44:57 -0300

    errou() roda apenas com letra errada

--- src/componentes/Letras.js
+++ src/componentes/Letras.js
@@ -1,12 +1,15 @@
 export default function Letras(props) {
 
   const btnLetra = (letra, index) => {
-    
     let ganhou = false;
     props.funcSelecionar(index);
-    props.errou();
+  
     console.log(letra);
     console.log(props.arrayPalavra);
+  
+    if (!props.arrayPalavra.includes(letra)) {
+      props.errou();
+    }
   
     const newArray = props.arrayUnderline.map((under, i) => {
       if (props.arrayPalavra[i] === letra) {
@@ -15,10 +18,10 @@ export default function Letras(props) {
         return under;
       }
     });
-
-    
+  
     props.setArrayUnderline(newArray);
   };
+  
   
   
   if(!props.arrayUnderline.includes("_")){
